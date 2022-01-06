f490a5e291cb340968eb884ad45fcffe5f7d8d8f
Neil Woodhouse <clonesupreme@gmail.com>
2022-01-06 14:21:57 +0000

    Added function to brute-force all alphabetic shift values

--- Python/caesar_shift.py
+++ Python/caesar_shift.py
@@ -22,14 +22,23 @@ def caesar_shift(plaintext, shift_value):
 
     return output
 
+def decrypt_check(ciphertext):
+    return_format = "Shift Val: {0}, Output: {1}"
+    for i in range(26):
+        print(return_format.format(i, caesar_shift(ciphertext, -i)))
+
 # Test function
 def main():
-    user_input = input("Input String: \n")
+    message = input("Input String: \n")
+    shift_val = int(input("Shift Value: \n"))
 
-    ciphered_string = caesar_shift(user_input, 14)
-    deciphered_string = caesar_shift(ciphered_string, -14)
+    ciphered_string = caesar_shift(message, shift_val)
+    deciphered_string = caesar_shift(ciphered_string, -shift_val)
 
     print("Ciphered String: " + ciphered_string)
     print("Deciphered String: " + deciphered_string)
 
+
+
 main()
+# decrypt_check()
